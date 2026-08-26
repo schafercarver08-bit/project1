c21344c5bbe39e6f05d3f41702dbdc36339983ea
schafercarver08-bit <schafer.carver08@gmail.com>
2026-08-26 18:10:18 +0000

    made a py file and a txt file, as well as a new directory

---
+++ hello.py
@@ -0,0 +1,2 @@
+userName = input("Hello, what is yourn name oddly dressed traveler: ")
+print(f"Hi there {userName}, where did you find such ugly and repulsive clothing. At first I thought you belonged to a traveling circus who kicked you out because you're so repulsive and could no longer stomach your presence.")
